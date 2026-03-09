54fd530dd8d8252915bc704e05beb838efeab212
Caaabooomm <85229948+gabriel-vitor-nave@users.noreply.github.com>
2026-03-09 15:49:39 -0300

    saving

--- ex-02-variaveis-tipos.js
+++ ex-02-variaveis-tipos.js
@@ -37,62 +37,62 @@ MÍNIMO DE RESPOSTAS NECESSÁRIAS: 8
 */
 
 /* Questão 01 */
-export let musicaFavorita
+export let musicaFavorita = "My Way - Frank Sinatra"
 
 /* Questão 02 */
-export let objetoQualquer
+export let objetoQualquer = ["maçã", "uva", "pera"]
 
 /* Questão 03 */
 export let valorIndefinido
 
 /* Questão 04 */
-export let tartarugasVoamVerdadeOuNao
+export let tartarugasVoamVerdadeOuNao = false
 
 /* Questão 05 */
-export let noveDivididoPorTres
+export let noveDivididoPorTres = 9 / 3
 
 /* Questão 06 */
-export let papagaiosFalamVerdadeOuNao
+export let papagaiosFalamVerdadeOuNao = false
 
 /* Questão 07 */
-export let umaFuncao
+export let umaFuncao = function() {}
 
 /* Questão 08 */
-export let cemDivididoPorCinco
+export let cemDivididoPorCinco = 100 / 5
 
 /* Questão 09 */
 
-export let nomeDaSuaEscola
+export let nomeDaSuaEscola = "ETE Cícero Dias"
 /* Questão 10 */
 
-export let quinzeMaisCinco
+export let quinzeMaisCinco = 15 + 5
 /* Questão 11 */
 
-export let valorFalso
+export let valorFalso = false
 /* Questão 12 */
 
-export let dezMaisQuatro
+export let dezMaisQuatro = 10 + 4
 
 /* Questão 13 */
-export let nomeDoSeuEstado
+export let nomeDoSeuEstado = "Pernambuco"
 
 /* Questão 14 */
-export let artistaFavorito
+export let artistaFavorito = "Alec'"
 
 /* Questão 15 */
-export let seteVezesSete
+export let seteVezesSete = 7 * 7
 
 /* Questão 16 */
-export let umMaiorQueDoisVerdadeOuNao
+export let umMaiorQueDoisVerdadeOuNao = 1 > 2
 
 /* Questão 17 */
-export let comidaFavorita
+export let comidaFavorita = "Estrogonofe"
 
 /* Questão 18 */
-export let oCeuEhAzulVerdadeOuNao
+export let oCeuEhAzulVerdadeOuNao = false
 
 /* Questão 19 */
-export let objetoNulo
+export let objetoNulo = null
 
 /* Questão 20 */
-export let valorVerdadeiro
+export let valorVerdadeiro = true
