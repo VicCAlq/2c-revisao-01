aeb810cd5be8fc1c28c912a2b237d302c7560626
caique-albuquerque-nave <caique.albuquerque@nave.org.br>
2026-03-09 16:19:58 -0300

    feito

--- ex-02-variaveis-tipos.js
+++ ex-02-variaveis-tipos.js
@@ -37,50 +37,50 @@ MÍNIMO DE RESPOSTAS NECESSÁRIAS: 8
 */
 
 /* Questão 01 */
-export let musicaFavorita
+export let musicaFavorita = "place- playboi carti"
 
 /* Questão 02 */
-export let objetoQualquer
+export let objetoQualquer 
 
 /* Questão 03 */
-export let valorIndefinido
+export let valorIndefinido = undefined
 
 /* Questão 04 */
-export let tartarugasVoamVerdadeOuNao
+export let tartarugasVoamVerdadeOuNao  = false
 
 /* Questão 05 */
-export let noveDivididoPorTres
+export let noveDivididoPorTres = 3
 
 /* Questão 06 */
-export let papagaiosFalamVerdadeOuNao
+export let papagaiosFalamVerdadeOuNao = true
 
 /* Questão 07 */
-export let umaFuncao
+export let umaFuncao = function Verde(){}
 
 /* Questão 08 */
-export let cemDivididoPorCinco
+export let cemDivididoPorCinco = 20
 
 /* Questão 09 */
 
-export let nomeDaSuaEscola
+export let nomeDaSuaEscola = "cicero dias" 
 /* Questão 10 */
 
-export let quinzeMaisCinco
+export let quinzeMaisCinco = 20 
 /* Questão 11 */
 
-export let valorFalso
+export let valorFalso = false
 /* Questão 12 */
 
-export let dezMaisQuatro
+export let dezMaisQuatro = 14
 
 /* Questão 13 */
-export let nomeDoSeuEstado
+export let nomeDoSeuEstado = "pernambuco"
 
 /* Questão 14 */
-export let artistaFavorito
+export let artistaFavorito = "Playboi Carti"
 
 /* Questão 15 */
-export let seteVezesSete
+export let seteVezesSete = 49
 
 /* Questão 16 */
 export let umMaiorQueDoisVerdadeOuNao
