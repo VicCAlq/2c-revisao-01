b062179f189c55a75b9022bf49451ac18f4f95a2
joaogomes-ctrl <joao.gomes@nave.org.br>
2026-03-09 15:38:09 -0300

    Correção do ultimo Commit( acabei enviando sem responder)

--- ex-02-variaveis-tipos.js
+++ ex-02-variaveis-tipos.js
@@ -37,64 +37,58 @@ MÍNIMO DE RESPOSTAS NECESSÁRIAS: 8
 */
 
 /* Questão 01 */
-export let musicaFavorita
+export let musicaFavorita = "505 - Articts Monkeys"
 
 /* Questão 02 */
-export let objetoQualquer
+export let objetoQualquer = { }
 
 /* Questão 03 */
-export let valorIndefinido
+export let valorIndefinido = undefined
 
 /* Questão 04 */
-export let tartarugasVoamVerdadeOuNao
+export let tartarugasVoamVerdadeOuNao = false
 
 /* Questão 05 */
-export let noveDivididoPorTres
+export let noveDivididoPorTres = 9 / 3
 
 /* Questão 06 */
-export let papagaiosFalamVerdadeOuNao
+export let papagaiosFalamVerdadeOuNao = true
 
 /* Questão 07 */
-export let umaFuncao
+export let umaFuncao = function () { return "sarve"; }
 
 /* Questão 08 */
-export let cemDivididoPorCinco
+export let cemDivididoPorCinco = 100 / 5
 
 /* Questão 09 */
-
-export let nomeDaSuaEscola
+export let nomeDaSuaEscola = "ETE Cicero Dias"
 /* Questão 10 */
-
-export let quinzeMaisCinco
+export let quinzeMaisCinco = 15 + 5
 /* Questão 11 */
-
-export let valorFalso
+export let valorFalso = false
 /* Questão 12 */
-
-export let dezMaisQuatro
+export let dezMaisQuatro = 10 + 4
 
 /* Questão 13 */
-export let nomeDoSeuEstado
+export let nomeDoSeuEstado = "Pernambuco"
 
 /* Questão 14 */
-export let artistaFavorito
+export let artistaFavorito = "MHRap"
 
 /* Questão 15 */
-export let seteVezesSete
+export let seteVezesSete = 7 * 7
 
 /* Questão 16 */
-export let umMaiorQueDoisVerdadeOuNao
+export let umMaiorQueDoisVerdadeOuNao = false
 
 /* Questão 17 */
-export let comidaFavorita
+export let comidaFavorita = "Pizza"
 
 /* Questão 18 */
-export let oCeuEhAzulVerdadeOuNao
+export let oCeuEhAzulVerdadeOuNao = true
 
 /* Questão 19 */
-export let objetoNulo
+export let objetoNulo = null
 
 /* Questão 20 */
-export let valorVerdadeiro
-
-
+export let valorVerdadeiro = true
