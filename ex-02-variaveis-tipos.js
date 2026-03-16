f0fc057a79703f11b83af7b7426147b33debc5d9
abner-ferreira-nave <abner.ferreira@nave.org.br>
2026-03-16 10:44:16 -0300

    entregue

--- ex-02-variaveis-tipos.js
+++ ex-02-variaveis-tipos.js
@@ -37,29 +37,29 @@ MÍNIMO DE RESPOSTAS NECESSÁRIAS: 8
 */
 
 /* Questão 01 */
-export let musicaFavorita
+export let musicaFavorita = filho meu
 
 /* Questão 02 */
-export let objetoQualquer
+export let objetoQualquer = cubo
 
 /* Questão 03 */
-export let valorIndefinido
+export let valorIndefinido = 45
 
 /* Questão 04 */
-export let tartarugasVoamVerdadeOuNao
+export let tartarugasVoamVerdadeOuNao = false
+
 
 /* Questão 05 */
-export let noveDivididoPorTres
+export let noveDivididoPorTres = 3
 
 /* Questão 06 */
-export let papagaiosFalamVerdadeOuNao
+export let papagaiosFalamVerdadeOuNao = true
 
 /* Questão 07 */
-export let umaFuncao
-
+export let umaFuncao = function (abrir porta){ return abrir porta;
+};
 /* Questão 08 */
-export let cemDivididoPorCinco
-
+export let cemDivididoPorCinco = 20
 /* Questão 09 */
 
 export let nomeDaSuaEscola
